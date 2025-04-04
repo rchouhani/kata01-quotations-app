27b5196d8cd056aac514e4874cf060c52695f150
rchouhani <rchouhani@me.com>
2025-04-04 15:58:03 +0200

    modify

--- script.js
+++ script.js
@@ -3,16 +3,26 @@ const quoteInput = document.querySelector('#quote')
 const authorInput = document.querySelector('#author')
 const buttonSend = document.querySelector('#buttonSend')
 
+
 formEntries.addEventListener('submit', (e)=>{
     e.preventDefault();
-    addQuote(quoteInput, authorInput)
+    const quote = document.querySelector('.quote')
+    const newParagraphQuote = document.createElement('p')
+    // newParagraphQuote.setAttribute('text')
+    const newParagraphAuthor = document.createElement('p')
+    // newParagraphAuthor.setAttribute('author')
+    newParagraphQuote.innerText = `Citation: "${quoteInput.value}"`
+    newParagraphQuote.style.fontStyle = 'italic'
+    newParagraphAuthor.innerText = `Auteur(trice): ${authorInput.value}`
+    quote.appendChild(newParagraphQuote)
+    quote.appendChild(newParagraphAuthor)
 })
 
-const addQuote = (quoteInput, authorInput) =>{
-    const newParagraphQuote = document.createElement('p')
-    newParagraphQuote.setAttribute('text')
-    const newParagraphAuthor = document.createElement('p')
-    newParagraphAuthor.setAttribute('author')
-    newParagraphQuote.innerText = quoteInput.value
-    newParagraphAuthor.innerText = authorInput.value
-}
+// const addQuote = (quoteInput, authorInput) =>{
+//     const newParagraphQuote = document.createElement('p')
+//     // newParagraphQuote.setAttribute('text')
+//     const newParagraphAuthor = document.createElement('p')
+//     // newParagraphAuthor.setAttribute('author')
+//     newParagraphQuote.innerText = quoteInput.value
+//     newParagraphAuthor.innerText = authorInput.value
+// }
